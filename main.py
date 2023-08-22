ba649522c54a2fe08f63d25d8c565f28ceb87cbe
Jrkinstein <119876817+Jrkinstein@users.noreply.github.com>
2023-08-22 20:45:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -72,6 +72,17 @@ def on_speak_button_click():
             break
         time.sleep(0)
 
+for root, dirs, files in os.walk("."):  # сканирование текущей директории и всех поддиректорий
+    for file in files:
+        if file.endswith(".mp3"):
+            mp3_files.append(os.path.join(root, file))  # добавление пути к файлу в массив
+for file in mp3_files:
+    media = instance.media_new_path(file)
+    playlist.add_media(media)
+    player.set_media(media)
+file = open('steam_path.txt', 'r')
+steam_path = file.read()
+file.close()
 # Функция для обработки текста и генерации ответа
 def process_text(text):
     if text == "Привет":
@@ -80,17 +91,39 @@ def process_text(text):
     elif text == "Как дела":
         return "У меня все хорошо, спасибо! А у тебя?"
 
-    elif text == "пока":
+    elif text.lower() == "пока":
         return "До свидания!"
 
     elif text.lower() =="открой youtube":
         webbrowser.open('www.youtube.com')
         return "Открываю"
 
-    elif text.lower() == "инфа о":
+    # пока не работает
+    elif text == "инфа о":
         search_term = text.lower().replace("инфа о", "")
         url = "https://www.google.com/search?q={}".format(search_term)
         webbrowser.open(url)
+        return "Ищу"
+
+    elif text.lower() == "открой переводчик":
+        webbrowser.open('https://translate.google.com/')
+        return "Открываю"
+
+    elif text.lower() == "открой steam":
+        os.startfile(steam_path, "open")
+        return "Открываю"
+
+    elif text.lower() ==('открой гонки' or 'открой forza' or 'открой форзу'):
+        app_command = f"steam://run/{1293830}"
+        os.startfile(steam_path, "open")
+        os.startfile(app_command)
+        return "Открываю"
+
+    elif text.lower() == 'открой шутер':
+        app_command = f"steam://run/{730}"
+        os.startfile(steam_path, "open")
+        os.startfile(app_command)
+        return "Открываю"
 
     else:
         return "Извините, я не понял."
